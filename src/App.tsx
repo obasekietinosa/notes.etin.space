cdb2c5dab66a3b0d2f9f444ac0c86598a13752a3
Etin Obaseki <etinosaobaseki@gmail.com>
2021-05-15 12:11:57 +0100

    style: Add Playfair display as global font

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
-import { Link, Router } from '@reach/router'
-import FancyDiv from 'components/FancyDiv'
-import Dynamic from 'containers/Dynamic'
-import './app.css'
+import React from "react";
+import { Root, Routes, addPrefetchExcludes, Head } from "react-static";
+import { Link, Router } from "@reach/router";
+import FancyDiv from "components/FancyDiv";
+import Dynamic from "containers/Dynamic";
+import "./app.css";
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
+addPrefetchExcludes(["dynamic"]);
 
 function App() {
   return (
     <Root>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <nav className="hidden">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
@@ -28,7 +35,7 @@ function App() {
         </FancyDiv>
       </div>
     </Root>
-  )
+  );
 }
 
-export default App
+export default App;
