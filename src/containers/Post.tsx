5ac8d6ae15d166a9b71e7df2fca9ba81e9da3a6e
Etin Obaseki <etinosaobaseki@gmail.com>
2021-05-16 21:03:10 +0100

    feat(Post): Replace content classes and style content

--- src/containers/Post.tsx
+++ src/containers/Post.tsx
@@ -6,19 +6,49 @@ import { siteConfig } from "../../utils";
 
 export default () => {
   const { post }: { post: Post } = useRouteData();
+  const content = post.content
+    .replaceAll("<h2", '<h2 class="text-2xl font-bold mb-4"')
+    .replaceAll("<h4", '<h4 class="text-lg font-semibold mb-4"')
+    .replaceAll("<p", '<p class="text-base mb-4"')
+    .replaceAll(
+      "<a",
+      `<a 
+        target="_blank"
+        rel="noreferrer noopener"
+        class="
+          transition duration-500 ease-in-out
+          px-1 
+          font-semibold
+          bg-gray-800 
+          text-white 
+          border-gray-800 
+          hover:border-b 
+          hover:bg-transparent 
+          hover:text-black"
+      `
+    );
+
   return (
     <>
       <Head>
-        <title>{post.title} - {siteConfig.title}</title>
+        <title>
+          {post.title} - {siteConfig.title}
+        </title>
         <meta name="description" content={post.excerpt} />
-        <meta property="og:title" content={`${post.title} - ${siteConfig.title}`} />
+        <meta
+          property="og:title"
+          content={`${post.title} - ${siteConfig.title}`}
+        />
         <meta property="og:description" content={post.excerpt} />
         <meta property="og:image" content={post.image} />
         <meta
           property="og:url"
           content={`https://notes.etin.space/posts/${post.slug}`}
         />
-        <meta name="twitter:title" content={`${post.title} - ${siteConfig.title}`} />
+        <meta
+          name="twitter:title"
+          content={`${post.title} - ${siteConfig.title}`}
+        />
         <meta name="twitter:description" content={post.excerpt} />
         <meta name="twitter:image" content={post.image} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -27,7 +57,7 @@ export default () => {
         <Link to="/">{"<"} Back</Link>
         <br />
         <h1 className="text-2xl mb-4">{post.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+        <div dangerouslySetInnerHTML={{ __html: content }} />
       </div>
     </>
   );
