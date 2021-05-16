32c86bb604a456d2cea8e955b404a9284065f0e2
Etin Obaseki <etinosaobaseki@gmail.com>
2021-05-16 21:02:39 +0100

    chore(utils): code formatting

--- utils.js
+++ utils.js
@@ -2,18 +2,23 @@ import he from "he";
 
 export const siteConfig = {
   title: "Etin's Notes",
-  description: "A Seed, learning to become a Tree. I write about the experiences that shape me."
-}
+  description:
+    "A Seed, learning to become a Tree. I write about the experiences that shape me.",
+};
 
-export const formatPosts = (posts) => posts.map(formatPost)
+export const formatPosts = (posts) => posts.map(formatPost);
 
 export const formatPost = (data) => ({
   title: he.decode(data.title),
   id: data.ID,
   datePublished: data.date,
-  image: data.featured_image?.length ? data.featured_image : `https://services.etin.space/notes/generate-image/?title=${encodeURI(data.title)}`,
-  categoryId: (Object.values(data.terms.category)[0])?.ID,
-  categoryName: (Object.values(data.terms.category)[0])?.name,
+  image: data.featured_image?.length
+    ? data.featured_image
+    : `https://services.etin.space/notes/generate-image/?title=${encodeURI(
+        data.title
+      )}`,
+  categoryId: Object.values(data.terms.category)[0]?.ID,
+  categoryName: Object.values(data.terms.category)[0]?.name,
   excerpt: he.decode(data.excerpt),
   content: data.content,
   author: formatAuthor(data.author),
