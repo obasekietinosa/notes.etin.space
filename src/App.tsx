460d3dc7995766b814db0f3a6918af5fbc83d80f
Etin Obaseki <etinosaobaseki@gmail.com>
2021-05-16 15:28:59 +0100

    feat(header): Add header and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Root, Routes, addPrefetchExcludes, Head } from "react-static";
 import { Link, Router } from "@reach/router";
-import FancyDiv from "components/FancyDiv";
 import Dynamic from "containers/Dynamic";
 import "./app.css";
 
@@ -18,21 +17,30 @@ function App() {
           rel="stylesheet"
         />
       </Head>
-      <nav className="hidden">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
-      <div className="content">
-        <FancyDiv>
-          <React.Suspense fallback={<em>Loading...</em>}>
-            <Router>
-              <Dynamic path="dynamic" />
-              <Routes path="*" />
-            </Router>
-          </React.Suspense>
-        </FancyDiv>
+      <header className="mb-8">
+        <nav className="flex flex-col md:flex-row justify-between items-center px-8 py-4">
+          <div className="text-center md:text-left mb-4 md:mb-0">
+            <h1 className="text-2xl">
+              <Link to="/">Etin's Notes</Link>
+            </h1>
+            <p className="text-xs">A collection of my scribblings</p>
+          </div>
+          <div className="flex justify-between w-full md:w-1/2 lg:w-1/3">
+            <Link className="text-black hover:text-opacity-60" to="/">Home</Link>
+            <Link className="text-black hover:text-opacity-60" to="/about">Categories</Link>
+            <Link className="text-black hover:text-opacity-60" to="/blog">Twitter</Link>
+            <Link className="text-black hover:text-opacity-60" to="/blog">Github</Link>
+          </div>
+        </nav>
+        <hr />
+      </header>
+      <div className="px-8 md:px-24 lg:px-48">
+        <React.Suspense fallback={<em>Loading...</em>}>
+          <Router>
+            <Dynamic path="dynamic" />
+            <Routes path="*" />
+          </Router>
+        </React.Suspense>
       </div>
     </Root>
   );
